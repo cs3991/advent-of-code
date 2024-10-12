198a6e607f6ee16e4ead24a00dd21eb078ebdfca
Cevilur <simardced31@gmail.com>
2024-10-12 18:57:34 +0200

    add 2023

---
+++ 2023/17.py
@@ -0,0 +1,23 @@
+from tictoc import tic, toc
+from cprint import cprint
+from collections import defaultdict, Counter
+
+
+with open("ex17.txt") as file:
+    ex = [line for line in file]
+
+with open("input17.txt") as file:
+    inp = [line for line in file]
+# print(inp)
+
+tic()
+# -------- Part 1 -----------
+print('   PART 1')
+
+
+toc('Part 1 done in')
+# -------- Part 2 -----------
+print('   PART 2')
+
+
+toc('Part 2 done in')
